e8fbf2cb591f2a380e7ac7f266aa5cac88ad64f7
Unknown <vatayo14@gmail.com>
2018-09-02 16:07:09 -0500

    OJALA

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ var express = require("express");
 var app = express();
 const ytdl = require("ytdl-core");
 const translate = require("google-translate-api");
+var youtubedl = require("youtube-dl");
+
+app.get("/youtube", function(req, res) {
+  const { url } = req.query;
+  var options = ["--username=user", "--password=hunter2"];
+  youtubedl.getInfo(url, options, function(err, info) {
+    if (err) throw err;
+
+    console.log("url:", info.url);
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify({ downloadUrl: info.url }, null, 4));
+  });
+});
 
 app.get("/youtubeDirect", function(req, res) {
   const { url } = req.query;
-  ytdl.getInfo(url, { debub: true }, (err, info) => {
+  ytdl.getInfo(url, { debub: true, downloadURL: true }, (err, info) => {
     if (err) {
       console.error(err.message);
       process.exit(1);
       return;
     }
-    let format = ytdl.chooseFormat(info.formats, {filter:'video'});
+    let format = ytdl.chooseFormat(info.formats, { filter: "audioonly" });
     if (format instanceof Error) {
       console.error(format.message);
       process.exit(1);
